7d3eb8a23e39210c843f4d9dfbc2caf6369ecc31
catraq <>
2024-06-17 11:16:28 +0200

    Removed implct function warning

--- include/nhgui_error.h
+++ include/nhgui_error.h
@@ -5,12 +5,13 @@
 #define S2(x) S1(x)
 #define LOCATION __FILE__ ":" S2(__LINE__) ":"
 
-#define CHECK_GL_ERROR() nhgui_gl_error(LOCATION)
 
 #include <stdio.h>
 
 #include "platform.h"
 
-void glError(const char *msg);
+void nhgui_gl_error(const char *msg);
+
+#define CHECK_GL_ERROR() nhgui_gl_error(LOCATION)
 
 #endif 
